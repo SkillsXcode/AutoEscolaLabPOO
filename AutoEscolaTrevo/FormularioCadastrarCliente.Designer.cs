df570d64d6ba5b24f7839de6ce4a70181b5d45a3
Abel Lucas <abellucasfonseca@gmail.com>
2022-02-19 18:01:40 -0300

    Imagem na Tela Cadastrar Cliente.

--- AutoEscolaTrevo/FormularioCadastrarCliente.Designer.cs
+++ AutoEscolaTrevo/FormularioCadastrarCliente.Designer.cs
@@ -46,9 +46,11 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = global::AutoEscolaTrevo.Properties.Resources.client_plus;
             this.pictureBox1.Location = new System.Drawing.Point(33, 28);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(126, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
