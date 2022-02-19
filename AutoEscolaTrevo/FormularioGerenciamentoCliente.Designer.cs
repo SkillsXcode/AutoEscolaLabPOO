01e6b24ce0ef93f7c6ba982febfa0ee578a69989
Abel Lucas <abellucasfonseca@gmail.com>
2022-02-18 22:38:26 -0300

    Tela de Gerenciamento de Cliente criada.

---
+++ AutoEscolaTrevo/FormularioGerenciamentoCliente.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace AutoEscolaTrevo
+{
+    partial class frmGerenciamentoCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbCliente = new System.Windows.Forms.PictureBox();
+            this.lblGerenciarClientes = new System.Windows.Forms.Label();
+            this.btnCadastrarCliente = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBuscarCliente = new System.Windows.Forms.Button();
+            this.btnEditarCliente = new System.Windows.Forms.Button();
+            this.btnExcluirCliente = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbCliente)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbCliente
+            // 
+            this.pcbCliente.Location = new System.Drawing.Point(24, 20);
+            this.pcbCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.pcbCliente.Name = "pcbCliente";
+            this.pcbCliente.Size = new System.Drawing.Size(102, 84);
+            this.pcbCliente.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcbCliente.TabIndex = 0;
+            this.pcbCliente.TabStop = false;
+            // 
+            // lblGerenciarClientes
+            // 
+            this.lblGerenciarClientes.AutoSize = true;
+            this.lblGerenciarClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.lblGerenciarClientes.Location = new System.Drawing.Point(242, 26);
+            this.lblGerenciarClientes.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblGerenciarClientes.Name = "lblGerenciarClientes";
+            this.lblGerenciarClientes.Size = new System.Drawing.Size(280, 37);
+            this.lblGerenciarClientes.TabIndex = 1;
+            this.lblGerenciarClientes.Text = "Gerenciar Clientes";
+            // 
+            // btnCadastrarCliente
+            // 
+            this.btnCadastrarCliente.BackColor = System.Drawing.Color.Black;
+            this.btnCadastrarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnCadastrarCliente.ForeColor = System.Drawing.Color.White;
+            this.btnCadastrarCliente.Location = new System.Drawing.Point(30, 169);
+            this.btnCadastrarCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCadastrarCliente.Name = "btnCadastrarCliente";
+            this.btnCadastrarCliente.Size = new System.Drawing.Size(150, 32);
+            this.btnCadastrarCliente.TabIndex = 2;
+            this.btnCadastrarCliente.Text = "+    Cadastrar";
+            this.btnCadastrarCliente.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 306);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(540, 266);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textBox1.Location = new System.Drawing.Point(30, 254);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(385, 24);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnBuscarCliente
+            // 
+            this.btnBuscarCliente.BackColor = System.Drawing.Color.Black;
+            this.btnBuscarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnBuscarCliente.ForeColor = System.Drawing.Color.White;
+            this.btnBuscarCliente.Location = new System.Drawing.Point(420, 247);
+            this.btnBuscarCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBuscarCliente.Name = "btnBuscarCliente";
+            this.btnBuscarCliente.Size = new System.Drawing.Size(150, 32);
+            this.btnBuscarCliente.TabIndex = 5;
+            this.btnBuscarCliente.Text = "Buscar";
+            this.btnBuscarCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnEditarCliente
+            // 
+            this.btnEditarCliente.BackColor = System.Drawing.Color.Black;
+            this.btnEditarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnEditarCliente.ForeColor = System.Drawing.Color.White;
+            this.btnEditarCliente.Location = new System.Drawing.Point(588, 474);
+            this.btnEditarCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEditarCliente.Name = "btnEditarCliente";
+            this.btnEditarCliente.Size = new System.Drawing.Size(150, 32);
+            this.btnEditarCliente.TabIndex = 6;
+            this.btnEditarCliente.Text = "Editar";
+            this.btnEditarCliente.UseVisualStyleBackColor = false;
+            // 
+            // btnExcluirCliente
+            // 
+            this.btnExcluirCliente.BackColor = System.Drawing.Color.Black;
+            this.btnExcluirCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluirCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.btnExcluirCliente.ForeColor = System.Drawing.Color.White;
+            this.btnExcluirCliente.Location = new System.Drawing.Point(588, 540);
+            this.btnExcluirCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExcluirCliente.Name = "btnExcluirCliente";
+            this.btnExcluirCliente.Size = new System.Drawing.Size(150, 32);
+            this.btnExcluirCliente.TabIndex = 7;
+            this.btnExcluirCliente.Text = "Excluir";
+            this.btnExcluirCliente.UseVisualStyleBackColor = false;
+            // 
+            // frmGerenciamentoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(754, 586);
+            this.Controls.Add(this.btnExcluirCliente);
+            this.Controls.Add(this.btnEditarCliente);
+            this.Controls.Add(this.btnBuscarCliente);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCadastrarCliente);
+            this.Controls.Add(this.lblGerenciarClientes);
+            this.Controls.Add(this.pcbCliente);
+            this.Name = "frmGerenciamentoCliente";
+            this.Text = "FormularioGerenciamentodeCliente";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbCliente)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbCliente;
+        private System.Windows.Forms.Label lblGerenciarClientes;
+        private System.Windows.Forms.Button btnCadastrarCliente;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBuscarCliente;
+        private System.Windows.Forms.Button btnEditarCliente;
+        private System.Windows.Forms.Button btnExcluirCliente;
+    }
+}
